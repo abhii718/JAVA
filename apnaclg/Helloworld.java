de1f30dc3859d5d2247e097f89e4a2a1f7eddefa
abhii718 <abhinaysingh47639@gamil.com>
2023-10-25 22:29:43 +0530

    j

---
+++ apnaclg/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, java!"); // This line prints "Hello, java!" to the console.
+        
+        // Variable
+        String name = "Aman"; // This line declares a String variable named "name" with the value "aman".
+
+        // Using the variable
+        System.out.println("My name is " + name); // This line uses the "name" variable.
+    }
+}
