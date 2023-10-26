4de455d1aa84e5f856015e80d4310355a60b31b0
abhii718 <abhinaysingh47639@gamil.com>
2023-10-26 14:40:32 +0530

    pattern

---
+++ apnaclg/patternt7number.java
@@ -0,0 +1,15 @@
+public class patternt7number {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        // int m = 5;
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
